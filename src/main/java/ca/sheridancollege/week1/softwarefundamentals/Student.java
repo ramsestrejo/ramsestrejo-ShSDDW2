60d4498c6e19b24dc99e2a38934f870de424928f
ramsesmindx <ramses@mindextent.com>
2019-05-17 13:20:58 -0400

    Adding code for Student and StudentList

--- src/main/java/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/main/java/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,4 +12,23 @@ package ca.sheridancollege.week1.softwarefundamentals;
  */
 public class Student {
     
+    
+    	private String name;
+
+	public Student( ) {
+		
+	}
+
+	public Student( String name ){
+		this.name = name;
+	}
+
+	public String getName( ) {
+		return this.name;
+	}
+
+	public void setName( String name ) {
+		this.name = name;
+	}
+    
 }
--- src/main/java/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/main/java/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -11,4 +11,35 @@ package ca.sheridancollege.week1.softwarefundamentals;
  */
 public class StudentList {
     
+    
+    	public static void main ( String args [ ] ) {
+		Student student1 = new Student( );
+		student1.setName( "Mark" );
+
+		Student student2 = new Student( );
+		student2.setName( "Andy" );
+
+		Student student3 = new Student( );
+		student3.setName( "Sukhpreet" );
+
+		Student student4 = new Student( );
+		student4.setName( "Charlie" );
+
+		Student student5 = new Student( );
+		student5.setName( "Tarik" );
+
+		Student [ ] students = new Student[ 5 ];
+		students[ 0 ] = student1;
+		students[ 1 ] = student2;
+		students[ 2 ] = student3;
+		students[ 3 ] = student4;
+		students[ 4 ] = new Student( "Pamela" );
+
+		// iterate through the array and print the names
+
+		for ( int i = 0 ; i < students.length ; i ++ ) {
+			System.out.println( "Name of the student is " + students[ i ].getName( ) );
+		}
+	} 
+    
 }
